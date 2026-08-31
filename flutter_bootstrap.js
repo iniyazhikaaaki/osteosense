025fbd533bd953ace88a46b158a87dce6f411338
OsteoSense Developer <developer@osteosense.local>
2026-08-31 13:29:45 +0530

    Deploy localized web release to GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"a804b261645ef8c13eb3d5c44a5c2fb0340c55
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3093727816" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2333164606" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
